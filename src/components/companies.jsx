d73e88f1e6a68c96fd4f8a0a5bab1e08b2221de3
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2020-12-12 20:51:44 +0100

    implement creation and deletion of companies

--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 
-import { useQuery, gql } from "@apollo/client"
+import { useMutation, useQuery, gql } from "@apollo/client"
 import { Link } from "react-router-dom"
+
+const CREATE_COMPANY = gql`
+  mutation CreateCompany($name: String!) {
+    createCompany(input: { name: $name }) {
+      company {
+        name
+        id
+      }
+    }
+  }
+`
+
+const DELETE_COMPANY = gql`
+  mutation DeleteCompany($id: ID!) {
+    deleteCompany(input: { id: $id }) {
+      ok
+    }
+  }
+`
 
 const COMPANIES = gql`
   query {
@@ -17,6 +36,90 @@ const COMPANIES = gql`
 `
 
 const Companies = () => {
+  const [name, setName] = useState("")
+  const changeName = ({ target: { value } }) => setName(value)
+
+  const [deleteCompany] = useMutation(DELETE_COMPANY)
+
+  const [createCompany, { loading: formLoading }] = useMutation(
+    CREATE_COMPANY
+    // { refetchQueries: [{ query: COMPANIES }] }
+    // {
+    //   update: (cache, { data: { createCompany } }) => {
+    //     console.log("cache", cache)
+    //     cache.modify({
+    //       fields: {
+    //         allCompanies: (existingCompanies = []) => {
+    //           console.log("edges", existingCompanies)
+    //           console.log("data", createCompany)
+    //           const newCompanyRef = cache.writeFragment({
+    //             data: createCompany,
+    //             fragment: gql`
+    //               fragment NewCompanyEdge on CompanyNodeEdge {
+    //                 node {
+    //                   id
+    //                   name
+    //                 }
+    //               }
+    //             `,
+    //           })
+    //           console.log([...existingCompanies, newCompanyRef])
+    //           return [...existingCompanies, newCompanyRef]
+    //         },
+    //       },
+    //     })
+    //   },
+    // }
+  )
+
+  const handleRemove = (id) => {
+    deleteCompany({
+      variables: { id },
+      update: (cache) => {
+        const data = cache.readQuery({ query: COMPANIES })
+        cache.writeQuery({
+          query: COMPANIES,
+          data: {
+            ...data,
+            allCompanies: {
+              ...data.allCompanies,
+              edges: data.allCompanies.edges.filter(
+                ({ node: { id: company_id } }) => company_id !== id
+              ),
+            },
+          },
+        })
+      },
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!/\S/.test(name)) {
+      return
+    }
+
+    createCompany({
+      variables: { name },
+      update: (cache, { data: { createCompany } }) => {
+        const data = cache.readQuery({ query: COMPANIES })
+        cache.writeQuery({
+          query: COMPANIES,
+          data: {
+            ...data,
+            allCompanies: {
+              ...data.allCompanies,
+              edges: [...data.allCompanies.edges, createCompany],
+            },
+          },
+        })
+      },
+    })
+    console.log(`add '${name}'`)
+    setName("")
+  }
+
   const { loading, error, data } = useQuery(COMPANIES)
 
   if (loading) return <p>Loading...</p>
@@ -25,13 +128,26 @@ const Companies = () => {
   return (
     <div>
       <h2>companies</h2>
-      {data.allCompanies.edges.map(({ node: { id, name } }) => (
-        <div key={id}>
-          <p>
-            <Link to={`/company/${id}`}>{id}</Link>: {name}
-          </p>
-        </div>
-      ))}
+      <div>
+        <p>add new company</p>
+        <form onSubmit={handleSubmit} disabled={formLoading}>
+          <label>
+            name: <input type="text" value={name} onChange={changeName} />
+          </label>{" "}
+          <input type="submit" value="add" />
+        </form>
+      </div>
+
+      <ul>
+        {data.allCompanies.edges.map(({ node: { id, name } }) => (
+          <li key={id}>
+            <p>
+              <Link to={`/company/${id}`}>{id}</Link>: {name}{" "}
+              <button onClick={() => handleRemove(id)}>remove</button>
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
