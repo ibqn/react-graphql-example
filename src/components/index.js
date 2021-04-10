7cc4066233a7ae016e132bf9817af56e4d647725
ibqn <evgen.ibqn@gmail.com>
2021-04-10 17:49:42 +0200

    export header in index

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Companies from "./companies"
 import Company from "./company"
 import Cars from "./cars"
 import Car from "./car"
+import Header from "./header"
 
-export { Home, Cars, Car, Companies, Company }
+export { Home, Cars, Car, Companies, Company, Header }
