60d939d065d18e069e9ceca0a405453c38c7d664
ibqn <evgen.ibqn@gmail.com>
2021-04-10 15:24:04 +0200

    remove header from home component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,11 +3,8 @@ import Header from "./header"
 
 const Home = () => (
   <>
-    <Header />
-    <div>
-      <h2>home</h2>
-      <p>please use the above navigation menu</p>
-    </div>
+    <h2>home</h2>
+    <p>please use the above navigation menu</p>
   </>
 )
 
