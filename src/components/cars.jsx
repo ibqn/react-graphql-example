df0fc285647f21fa9b85975159b6cd71ab7f4943
ibqn <evgen.ibqn@gmail.com>
2021-04-10 17:24:42 +0200

    improve cars component

--- src/components/cars.jsx
+++ src/components/cars.jsx
@@ -1,10 +1,8 @@
-import React from "react"
-
 import { useQuery, gql } from "@apollo/client"
 import { Link } from "react-router-dom"
 
 const CARS = gql`
-  query {
+  query Cars {
     allCars {
       edges {
         node {
@@ -23,10 +21,10 @@ const Cars = () => {
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  const cars = data.allCars.edges.map(({ node }) => node)
+  const cars = data?.allCars?.edges.map(({ node }) => node)
 
   return (
-    <div>
+    <>
       <h2>cars</h2>
       {/* {JSON.stringify(cars)} */}
       {cars.map(({ id, name, notes }) => (
@@ -34,7 +32,7 @@ const Cars = () => {
           <Link to={`/car/${id}`}>{name}</Link> notes: {notes}
         </p>
       ))}
-    </div>
+    </>
   )
 }
 
